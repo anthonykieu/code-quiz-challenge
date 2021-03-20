4b1819164bd336878a743cc40211823842c6c462
Anthony Kieu <anthonykieu@gmail.com>
2021-03-20 10:46:09 -0400

    added the quiz question portion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 var questions = [
     {
-q: "",
-o: "",
-c: "",
-}
+        q: "Javascript is used to style the application",
+        b: ["true", "false"],
+        a: 1
+    },
+    {
+        q: "CSS is used to style the application",
+        b: ["true", "false"],
+        a: 0
+    },
+    {
+        q: "Arrays can contain:",
+        b: ["strings", "objects", "arrays", "numbers", "all of the above"],
+        a: 4
+    },
+    {
+        q: "Which is not a common data type:",
+        b: ["wigets", "numbers", "strings", "booleans"],
+        a: 4
+    },
+    {
+        q: "Consoling is a useful to for troubleshooting",
+        b: ["true", "false"],
+        a: 0
+    },
 
 ];
 
@@ -12,24 +32,25 @@ var currentQuestion = 0
 function startQuiz() {
     document.getElementById("quiz").style = "display: none";
     document.getElementById("quizQuestion").style = "display: block";
+    document.getElementById("quizQuestionDiv").style = "display:block";
     showQuestion(currentQuestion)
 }
 function showQuestion(index) {
 
     var question = questions[index]
-    
+
     document.getElementById("question").textContent = question.q
     document.getElementById("answer").innerHTML = ''
     for (let i = 0; i < question.b.length; i++) {
         var button = document.createElement("button")
-        button.textContent = question.o[i]
+        button.textContent = question.b[i]
         button.style.display = "block"
         button.addEventListener("click", function () {
-            checkAnswer(i === question.c)
+            checkAnswer(i === question.a)
         })
         document.getElementById("answer").appendChild(button)
     }
-    
+
 }
 
 function checkAnswer(correct) {
@@ -42,8 +63,8 @@ function checkAnswer(correct) {
     currentQuestion++
 
     document.getElementById("responseBlock").style = "display:block"
-     
-    ;
+
+        ;
 
     if (currentQuestion < questions.length) {
         showQuestion(currentQuestion)
