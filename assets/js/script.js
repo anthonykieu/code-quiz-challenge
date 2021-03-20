be43a9d8ec9dfc71f8bb90079fa2da0b74e6f240
Anthony Kieu <anthonykieu@gmail.com>
2021-03-19 23:59:19 -0400

    added question section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,56 @@
-function startQuiz() {
-    document.getElementById("quiz").style = "display: none";
+var questions = [
+    {
+q: "",
+o: "",
+c: "",
 }
 
+];
 
+var currentQuestion = 0
+
+function startQuiz() {
+    document.getElementById("quiz").style = "display: none";
+    document.getElementById("quizQuestion").style = "display: block";
+    showQuestion(currentQuestion)
+}
+function showQuestion(index) {
+
+    var question = questions[index]
+    
+    document.getElementById("question").textContent = question.q
+    document.getElementById("answer").innerHTML = ''
+    for (let i = 0; i < question.b.length; i++) {
+        var button = document.createElement("button")
+        button.textContent = question.o[i]
+        button.style.display = "block"
+        button.addEventListener("click", function () {
+            checkAnswer(i === question.c)
+        })
+        document.getElementById("answer").appendChild(button)
+    }
+    
+}
+
+function checkAnswer(correct) {
+
+    if (correct) {
+        document.getElementById("response").textContent = "Correct"
+    } else {
+        document.getElementById("response").textContent = "Incorrect"
+    }
+    currentQuestion++
+
+    document.getElementById("responseBlock").style = "display:block"
+     
+    ;
+
+    if (currentQuestion < questions.length) {
+        showQuestion(currentQuestion)
+
+    } else {
+
+        console.log("Quiz complete")
+    }
+};
 document.getElementById("startBtn").addEventListener("click", startQuiz)
